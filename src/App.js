2315a433e99505e0a3ebda4c9b2074d566cdeeec
sharmas75 <71354509+sharmas75@users.noreply.github.com>
2023-11-21 14:32:50 -0700

    remove animate presence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
 function getRandomQuote(quotes) {
