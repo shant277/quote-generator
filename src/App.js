22dfaf07473ea4c308d6c3700be3933cbc62aca5
sharmas75 <71354509+sharmas75@users.noreply.github.com>
2023-11-21 14:32:27 -0700

    add quote functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                  Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
-export default App;
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+  const appVariants = {
+    hover: {
+      scale: 1.2,
+      transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 10,
+        duration: 0.5,
+      },
+    },
+    tap: { scale: 0.9 },
+  };
+  const quoteVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, duration: 2 },
+    exit: { opacity: 0 },
+  };
+
+  return (
+    <motion.main>
+      <h1>Project 3: Quote Generator</h1>
+      <section>
+        <motion.button
+          onClick={getNewQuote}
+          variants={appVariants}
+          whileHover="hover"
+          whileTap="tap"
+        >
+          New Quote
+        </motion.button>
+        <motion.h3
+          key={quote?.text}
+          variants={quoteVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <span>“</span>
+          {quote?.text}
+          <span>“</span>
+        </motion.h3>
+        {<i>- {quote?.author}</i>}
+      </section>
+    </motion.main>
+  );
+}
